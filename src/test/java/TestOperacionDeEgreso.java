8bff73142c9c591a6c945c86ecbe38fea63b4563
Gonzalo <gebonora@gmail.com>
2020-05-11 01:23:06 -0300

    borro DummyDesign y creo template para OperacionDeEgreso con su test

---
+++ src/test/java/TestOperacionDeEgreso.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestOperacionDeEgreso {
+    OperacionDeEgreso operacionDeEgreso = new OperacionDeEgreso();
+
+    @Test
+    public void testBlahblah() {
+        assertEquals(1, 1);
+    }
+}
