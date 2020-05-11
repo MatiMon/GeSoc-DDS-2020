a61aa4d477768019c16e1d627138b0f46accae23
Gonzalo <gebonora@gmail.com>
2020-05-11 01:18:10 -0300

    borro DummyDesign y subo un template de test

---
+++ src/test/java/TestItem.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestItem {
+    Item item = new Item();
+
+    @Test
+    public void testBlablabla() {
+        assertEquals(1, 1);
+    }
+
+
+}
