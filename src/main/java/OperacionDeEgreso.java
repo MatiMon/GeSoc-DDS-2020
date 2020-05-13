b00b406be5b26595093ffb4156509ce124584766
dario <dario.echazarreta@outlook.com>
2020-05-13 03:04:36 -0300

    Agregada OperacionDeEgreso
    Agregada OperacionDeEgreso y clases relacionadas.
    Agregado al pom dependencia para que funcione Pair

--- src/main/java/OperacionDeEgreso.java
+++ src/main/java/OperacionDeEgreso.java
@@ -1,2 +1,20 @@
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+
 public class OperacionDeEgreso {
+	Pair<TipoDocumentoComercial,Integer> DocumentoContable;
+	Proveedor proveedor;
+	Date f_Operacion;
+	List<Item> items;
+	MedioPago pago;
+	Entidad entidad;
+	
+	public Integer valorTotal() {
+		int sum=0;
+		for(int i = 0; i < items.size(); i++)
+		    sum += items.get(i).valorItem();
+		return sum;
+	}
 }
---
+++ src/main/java/TipoDocumentoComercial.java
@@ -0,0 +1,5 @@
+
+public enum TipoDocumentoComercial {
+		Factura,Ticket
+		//no se si es necesario agregar alguno mas
+}
