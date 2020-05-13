348ce071c9bc07adf5670c8cd33ff1935ac6149b
Gonzalo <gebonora@gmail.com>
2020-05-13 02:37:29 -0300

    refactor para ValidarMilMasUsadas

--- src/main/java/ValidarMilMasUsadas.java
+++ src/main/java/ValidarMilMasUsadas.java
@@ -4,24 +4,27 @@ public class ValidarMilMasUsadas implements ValidadorPasswords{
     String nombreArchivo = "/10k-most-common.txt";
     @Override
     public boolean validarPassword(String password){
+        return !passwordEnArchivo(password,this.nombreArchivo);
+    }
+    private boolean passwordEnArchivo(String unaPassword, String unNombreArchivo){
         String strLine;
-        InputStream inputStream = getClass().getResourceAsStream(nombreArchivo);
+        InputStream inputStream = getClass().getResourceAsStream(unNombreArchivo);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         try {
             while ((strLine = reader.readLine()) != null) {
-                if (password.equals(strLine)) {
+                if (unaPassword.equals(strLine)) {
                     reader.close();
                     inputStream.close();
-                    return false;
+                    return true;
                 }
             }
             reader.close();
             inputStream.close();
-            return true;
+            return false;
         }
         catch(IOException ioe){
             ioe.printStackTrace();
         }
-        return false;
+        return true;
     }
 }
