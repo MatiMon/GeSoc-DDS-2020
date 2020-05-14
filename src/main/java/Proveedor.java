e79e8f6950c01fee7b73c224c8102e235da54575
LucasUsh <lucaas-e@hotmail.com>
2020-05-13 22:23:35 -0300

    Proveedor listo

--- src/main/java/Proveedor.java
+++ src/main/java/Proveedor.java
@@ -2,4 +2,33 @@ public class Proveedor {
 	public String razonSocial;
 	public Direccion direccion;
 	public int codigoUnicoDeIdentificacion; // DNI, CUIT o CUIL
+	
+	//Constructor:
+	public Proveedor (String razonSocial, Direccion direccion, int codigoUnicoDeIdentificacion) {
+		this.razonSocial = razonSocial;
+		this.direccion = direccion;
+		this.codigoUnicoDeIdentificacion = codigoUnicoDeIdentificacion;
+	}
+	
+	//Getters/Setters
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getCodigoUnicoDeIdentificacion() {
+		return codigoUnicoDeIdentificacion;
+	}
+
 }
