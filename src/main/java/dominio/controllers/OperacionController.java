ccb56d5cfc12704c037231a9ae83a42df5c38099
Gonzalo Bonora <gebonora@gmail.com>
2020-12-02 23:05:37 -0300

    avances

--- src/main/java/dominio/controllers/OperacionController.java
+++ src/main/java/dominio/controllers/OperacionController.java
@@ -27,6 +27,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class OperacionController extends Controller {
@@ -73,83 +74,90 @@ public class OperacionController extends Controller {
 
     public ModelAndView nuevaOperacionItems(Request request, Response response) {
         Map<String, Object> parametros = this.getSessionParams(request);
-        if (request.queryParams("numeroDocumento") != null) {
-            int nroDocumento = Integer.parseInt(request.queryParams("numeroDocumento"));
-            builder.setNumeroDocumentoComercial(nroDocumento);
-        }
-        if (request.queryParams("cantidadPresupuestos") != null) {
-            int cantidadPresupuestos = Integer.parseInt(request.queryParams("cantidadPresupuestos"));
-            builder.setCantidadDePresupuestosRequeridos(cantidadPresupuestos);
-        }
-        if (request.queryParams("proveedorId") != null) {
-            long idProveedor = Long.parseLong(request.queryParams("proveedorId"));
-            Repositorio<Proveedor> repoProveedor = FactoryRepositorio.get(Proveedor.class);
-            Proveedor proveedor = repoProveedor.buscar(idProveedor);
-            builder.setProveedor(proveedor);
-        }
-        if (request.queryParams("monedaId") != null) {
-            long idMoneda = Long.parseLong(request.queryParams("monedaId"));
-            Repositorio<Moneda> repoMonedas = FactoryRepositorio.get(Moneda.class);
-            Moneda moneda = repoMonedas.buscar(idMoneda);
-            builder.setMoneda(moneda);
-        }
-        if (request.queryParams("entidadId") != null) {
-            long idEntidad = Long.parseLong(request.queryParams("entidadId"));
-            if (esBase) {
-                EntidadBase entidadBase = repoEntidadesBase.buscar(idEntidad);
-                builder.setEntidad(entidadBase);
-            } else {
-                EntidadJuridica entidadJuridica = repoEntidadesJuridicas.buscar(idEntidad);
-                builder.setEntidad(entidadJuridica);
+        try {
+            if (request.queryParams("numeroDocumento") != null) {
+                int nroDocumento = Integer.parseInt(request.queryParams("numeroDocumento"));
+                builder.setNumeroDocumentoComercial(nroDocumento);
             }
-        }
-        if (request.queryParams("tipoDocumento") != null) {
-            String tipoDoc = request.queryParams("tipoDocumento");
-            if (tipoDoc.equals("factura")) {
-                this.builder.setTipoDocumentoComercial(TipoDocumentoComercial.Factura);
+            if (request.queryParams("cantidadPresupuestos") != null) {
+                int cantidadPresupuestos = Integer.parseInt(request.queryParams("cantidadPresupuestos"));
+                builder.setCantidadDePresupuestosRequeridos(cantidadPresupuestos);
             }
-            if (tipoDoc.equals("ticket")) {
-                this.builder.setTipoDocumentoComercial(TipoDocumentoComercial.Ticket);
+            if (request.queryParams("proveedorId") != null) {
+                long idProveedor = Long.parseLong(request.queryParams("proveedorId"));
+                Repositorio<Proveedor> repoProveedor = FactoryRepositorio.get(Proveedor.class);
+                Proveedor proveedor = repoProveedor.buscar(idProveedor);
+                builder.setProveedor(proveedor);
             }
-        }
+            if (request.queryParams("monedaId") != null) {
+                long idMoneda = Long.parseLong(request.queryParams("monedaId"));
+                Repositorio<Moneda> repoMonedas = FactoryRepositorio.get(Moneda.class);
+                Moneda moneda = repoMonedas.buscar(idMoneda);
+                builder.setMoneda(moneda);
+            }
+            if (request.queryParams("entidadId") != null) {
+                long idEntidad = Long.parseLong(request.queryParams("entidadId"));
+                if (esBase) {
+                    EntidadBase entidadBase = repoEntidadesBase.buscar(idEntidad);
+                    builder.setEntidad(entidadBase);
+                } else {
+                    EntidadJuridica entidadJuridica = repoEntidadesJuridicas.buscar(idEntidad);
+                    builder.setEntidad(entidadJuridica);
+                }
+            }
+            if (request.queryParams("tipoDocumento") != null) {
+                String tipoDoc = request.queryParams("tipoDocumento");
+                if (tipoDoc.equals("factura")) {
+                    this.builder.setTipoDocumentoComercial(TipoDocumentoComercial.Factura);
+                }
+                if (tipoDoc.equals("ticket")) {
+                    this.builder.setTipoDocumentoComercial(TipoDocumentoComercial.Ticket);
+                }
+            }
 
-        if (request.queryParams("tipoMedioPago") != null) {
-            String medioDePago = request.queryParams("tipoMedioPago");
-            switch (medioDePago) {
-                case "efectivo":
-                    Efectivo efectivo = FactoryRepositorio.get(Efectivo.class).buscarTodos().get(0);
-                    builder.setMetodoPago(efectivo);
-                    break;
-                case "tarjetaCredito":
-                    Tarjeta tarjetaCredito = FactoryRepositorio.get(Tarjeta.class).buscarTodos()
-                            .stream().filter(tarjeta -> tarjeta.getTipo() == TipoTarjeta.tarjetaCredito)
-                            .collect(Collectors.toList()).get(0);
-                    builder.setMetodoPago(tarjetaCredito);
-                    break;
-                case "tarjetaDebito":
-                    Tarjeta tarjetaDebito = FactoryRepositorio.get(Tarjeta.class).buscarTodos()
-                            .stream().filter(tarjeta -> tarjeta.getTipo() == TipoTarjeta.tarjetaDebito)
-                            .collect(Collectors.toList()).get(0);
-                    builder.setMetodoPago(tarjetaDebito);
-                    break;
-                default:
-                    break;
+            if (request.queryParams("tipoMedioPago") != null) {
+                String medioDePago = request.queryParams("tipoMedioPago");
+                switch (medioDePago) {
+                    case "efectivo":
+                        Efectivo efectivo = FactoryRepositorio.get(Efectivo.class).buscarTodos().get(0);
+                        builder.setMetodoPago(efectivo);
+                        break;
+                    case "tarjetaCredito":
+                        Tarjeta tarjetaCredito = FactoryRepositorio.get(Tarjeta.class).buscarTodos()
+                                .stream().filter(tarjeta -> tarjeta.getTipo() == TipoTarjeta.tarjetaCredito)
+                                .collect(Collectors.toList()).get(0);
+                        builder.setMetodoPago(tarjetaCredito);
+                        break;
+                    case "tarjetaDebito":
+                        Tarjeta tarjetaDebito = FactoryRepositorio.get(Tarjeta.class).buscarTodos()
+                                .stream().filter(tarjeta -> tarjeta.getTipo() == TipoTarjeta.tarjetaDebito)
+                                .collect(Collectors.toList()).get(0);
+                        builder.setMetodoPago(tarjetaDebito);
+                        break;
+                    default:
+                        break;
+                }
             }
+            return new ModelAndView(parametros, "nueva-operacion-items.hbs");
+        } catch (Exception e) {
+            return nuevaOperacion(request, response);
         }
-
-        return new ModelAndView(parametros, "nueva-operacion-items.hbs");
     }
 
     public ModelAndView nuevaOperacionFinalizar(Request request, Response response) {
-        Map<String, Object> parametros = this.getSessionParams(request);
-        String descripcion = request.queryParams("descripcion");
-        Double precio = Double.parseDouble(request.queryParams("precio"));
-        int cantidad = Integer.parseInt(request.queryParams("cantidad"));
-        Producto producto = new Producto(descripcion, descripcion, precio);
+        try {
+            Map<String, Object> parametros = this.getSessionParams(request);
+            String descripcion = request.queryParams("descripcion");
+            Double precio = Double.parseDouble(request.queryParams("precio"));
+            int cantidad = Integer.parseInt(request.queryParams("cantidad"));
+            Producto producto = new Producto(descripcion, descripcion, precio);
 
-        builder.agregarItem(producto, cantidad);
+            builder.agregarItem(producto, cantidad);
 
-        return new ModelAndView(parametros, "nueva-operacion-finalizar.hbs");
+            return new ModelAndView(parametros, "nueva-operacion-finalizar.hbs");
+        } catch (Exception e) {
+            return nuevaOperacion(request, response);
+        }
     }
 
     public ModelAndView crearOperacion(Request request, Response response) {
