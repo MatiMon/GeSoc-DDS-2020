83b207366b9f0e8c34a95f2342c80e97a14fb9c7
Gonzalo Bonora <gebonora@gmail.com>
2020-12-04 00:48:46 -0300

    merge

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -29,16 +29,6 @@ public class Router {
         Router.configure();
     }
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
-
-
     private static void configure() throws InvalidKeySpecException, NoSuchAlgorithmException {
         MainController mainController = new MainController();
         LoginController loginController = new LoginController();
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,8 +8,17 @@ import java.security.spec.InvalidKeySpecException;
 
 public class Server {
     public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException {
-        Spark.port(9000);
+        Spark.port(getHerokuAssignedPort());
         Router.init();
         DebugScreen.enableDebugScreen();
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 9000; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
